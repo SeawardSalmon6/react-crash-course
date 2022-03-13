31cad449d015ab8565888f2e7d59ab40a138f59f
SeawardSalmon6 <rayhanachour@hotmail.com>
2022-03-12 22:05:03 -0300

    feat: adding navigation bar to the app

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Routes, Route } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups/index.js";
 import FavoritesPage from "./pages/Favorites/index.js";
 import NewMeetupPage from "./pages/NewMeetup/index.js";
+import MainNavigation from "./components/layout/MainNavigation/index.js";
 
 function App() {
   return (
     <div>
+      <MainNavigation />
       <Routes>
         <Route path="/" element={<AllMeetupsPage />} />
         <Route path="/new-meetup" element={<NewMeetupPage />} />
