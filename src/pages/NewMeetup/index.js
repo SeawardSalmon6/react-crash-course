2e17ad2924df17f9beb7873f2d32a93e6982cbd4
SeawardSalmon6 <rayhanachour@hotmail.com>
2022-03-13 12:30:40 -0300

    feat: connecting application to firebase api and fetching data dynamically

--- src/pages/NewMeetup/index.js
+++ src/pages/NewMeetup/index.js
@@ -1,10 +1,29 @@
+import { useNavigate } from "react-router-dom";
+
 import NewMeetupForm from "../../components/Meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+  const navigate = useNavigate();
+
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-meetups-ss6-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        header: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(() => {
+      navigate("/", { replace: true });
+    });
+  }
+
   return (
     <section>
       <h1>New Meetup Page</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
